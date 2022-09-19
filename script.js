395d765cd77b01b16e72508228cc68946e8895e3
mustafadede <mustafa.dede.0016@gmail.com>
2022-09-19 17:35:35 +0300

    form fix

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const player1 = document.querySelector('.player1');
 const player2 = document.querySelector('.player2');
 const form = document.getElementsByTagName('form');
 const sumbit = document.querySelector('.submit');
-console.log(form);
-console.log(player1.value);
-console.log(player2.value);
 
 sumbit.addEventListener('click', function () {
   localStorage.setItem('player1', player1.value);
